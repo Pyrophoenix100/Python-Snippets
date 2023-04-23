606af283da289d6e5978ac3e565c937a9fda4da2
Pyrophoenix100 <alectuc@gmail.com>
2023-04-23 12:20:58 -0500

    asdf

--- JavaScript/includes.js
+++ JavaScript/includes.js
@@ -20,7 +20,7 @@
  *      <p> Name: {{ first-name }} {{ last-name }} </p>
  **/
 
-/* Version is V0.06 */
+/* Version is V0.05 */
 window.addEventListener("load", (event) => {
     let includes = document.getElementsByTagName("include");
     var includeElem = document.getElementsByTagName('include')[document.getElementsByTagName('include').length - 1];
