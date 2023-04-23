7a328c6202a0bb5c06e8ba223fe76658759e1e2d
Pyrophoenix100 <alectuc@gmail.com>
2023-04-23 12:12:41 -0500

    sanity

--- JavaScript/includes.js
+++ JavaScript/includes.js
@@ -20,7 +20,7 @@
  *      <p> Name: {{ first-name }} {{ last-name }} </p>
  **/
 
-/* Version is V0.09 */
+/* Version is V0.08 */
 window.addEventListener("load", (event) => {
     let includes = document.getElementsByTagName("include");
     var includeElem = document.getElementsByTagName('include')[document.getElementsByTagName('include').length - 1];
