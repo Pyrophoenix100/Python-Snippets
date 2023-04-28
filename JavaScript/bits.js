cded0ca89c4467569e4bf539c67b32c445293793
Pyro100 <alectuc@gmail.com>
2023-04-28 02:45:22 -0500

    Added TypingText to bits.js

--- JavaScript/bits.js
+++ JavaScript/bits.js
@@ -1,6 +1,24 @@
 /* Bits.js - Alec Tuchscherer - 2023 */
 // Bits.js is a small library file for bits of js code that don't deserve their own file (yet)
 
-function typingText() {
-    
+//When called, makes all elements with the class 'typed' type out with the specified duration
+function typingText(delay, leader="_") {
+    const typedElements = document.getElementsByClassName("typed");
+    for (let i = 0; i < typedElements.length; i++) {
+        const text = typedElements[i].textContent;
+        typedElements[i].textContent = "";
+        typeText(typedElements[i], text, delay, leader);
+    }
+}
+
+async function typeText(element, text, delay, leader) {
+    for (let i = 0; i < text.length; i++) {
+        element.textContent += text.charAt(i) + leader;
+        await sleep(delay);
+        element.textContent = element.textContent.slice(0, i+1)
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
