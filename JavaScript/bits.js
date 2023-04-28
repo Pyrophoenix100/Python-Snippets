6f19759ab4f90398fd5613e45230d0ab1b53da5f
Pyro100 <alectuc@gmail.com>
2023-04-28 02:51:22 -0500

    It's 3:00?

--- JavaScript/bits.js
+++ JavaScript/bits.js
@@ -1,7 +1,8 @@
 /* Bits.js - Alec Tuchscherer - 2023 */
 // Bits.js is a small library file for bits of js code that don't deserve their own file (yet)
 
-//When called, makes all elements with the class 'typed' type out with the specified duration
+// When called, makes all elements with the class 'typed' type out with the specified duration.
+// Recommended to put in a document.onload() 
 function typingText(delay, leader="_") {
     const typedElements = document.getElementsByClassName("typed");
     for (let i = 0; i < typedElements.length; i++) {
@@ -10,7 +11,6 @@ function typingText(delay, leader="_") {
         typeText(typedElements[i], text, delay, leader);
     }
 }
-
 async function typeText(element, text, delay, leader) {
     for (let i = 0; i < text.length; i++) {
         element.textContent += text.charAt(i) + leader;
@@ -18,7 +18,6 @@ async function typeText(element, text, delay, leader) {
         element.textContent = element.textContent.slice(0, i+1)
     }
 }
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
